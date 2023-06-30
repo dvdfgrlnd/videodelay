3fcc0d5ebc5eb7aca67f677505a5a60a9c4f0f90
davidfagerlund <dvdfgrlnd@gmail.com>
2023-06-30 20:31:57 +0200

    Add delay input slider

--- main.js
+++ main.js
@@ -3,48 +3,81 @@ var started = false;
 var log = msg => console.log(msg); // document.querySelector("#logdiv").innerHTML += "<br>" + msg;
 
 
-let window_height = document.documentElement.clientHeight;
-let window_width = document.documentElement.clientWidth;
-let aspect_ratio = window_width / window_height;
-navigator.mediaDevices.getUserMedia({ video: {
-  facingMode: "user",
-  aspectRatio: 1,
-  min: 24,  // very important to define min value here
-  ideal: 24,
-  max: 25,
-} })
-  .then(async (stream) => {
-    const delay = 2000;
-    const mimeType = `video/webm; codecs="vp9"`;
-    const mediaSource = new MediaSource();
-    var video = document.querySelector("video");
-    video.src = URL.createObjectURL(mediaSource);
-    await new Promise((res) =>
-      mediaSource.addEventListener("sourceopen", res, { once: true })
-    );
-    const sourceBuffer = mediaSource.addSourceBuffer(mimeType);
-    const recorder = new MediaRecorder(stream, { mimeType });
-    recorder.ondataavailable = async ({ data }) => {
-      if (mediaSource.readyState !== "open" || !data.size) {
-        return;
-      }
-      sourceBuffer.appendBuffer(await data.arrayBuffer());
-    };
-    sourceBuffer.addEventListener("update", ()=>{
-      if (
-          video.buffered.length &&
-          video.buffered.end(0) - video.buffered.start(0) > 35
-      )
-      {
-          let diff = video.buffered.end(0) - video.buffered.start(0);
-          // Can't remove if video is short
-          sourceBuffer.remove(0, video.buffered.end(0) - 35)
-          log(`REMOVED. ${diff}`);
-      }
-    });
-    video.pause();
-    recorder.start(50);
-    setTimeout(() => video.play(), delay);
-  })
-  .catch(log);
+var recorder = null;
 
+const delay_input = document.querySelector("#delayseconds");
+const delay_value = document.querySelector("#delayvalue");
+delay_value.textContent = delay_input.value;
+delay_input.addEventListener("input", (event) => {
+  delay_value.textContent = event.target.value;
+});
+
+var deadlineTimeout = null;
+delay_input.addEventListener("change", (event) => {
+  if(recorder) {
+    if(deadlineTimeout){
+      clearTimeout(deadlineTimeout);
+    }
+    deadlineTimeout = setTimeout(()=>{
+      var video = document.querySelector("video");
+      video.pause();
+      recorder.stop();
+
+      const delay_seconds = parseInt(event.target.value)
+      log(delay_seconds);
+      startRecording(delay_seconds);
+    }, 2000);
+  }
+});
+
+
+
+let window_height = document.querySelector("#videocontainer").clientHeight;
+let window_width = document.querySelector("#videocontainer").clientWidth;
+console.log(window_height, window_width);
+let aspect_ratio = window_width / window_height;
+
+async function startRecording(delaySeconds) {
+  let delay = delaySeconds * 1000
+  let stream = await navigator.mediaDevices.getUserMedia({
+    video: {
+      facingMode: "user",
+      aspectRatio: aspect_ratio,
+      min: 24,  // very important to define min value here
+      ideal: 24,
+      max: 25,
+    }
+  });
+  const mimeType = `video/webm; codecs="vp9"`;
+  const mediaSource = new MediaSource();
+  var video = document.querySelector("video");
+  video.src = URL.createObjectURL(mediaSource);
+  await new Promise((res) =>
+    mediaSource.addEventListener("sourceopen", res, { once: true })
+  );
+  const sourceBuffer = mediaSource.addSourceBuffer(mimeType);
+  sourceBuffer.mode = "sequence";
+  recorder = new MediaRecorder(stream, { mimeType });
+  recorder.ondataavailable = async ({ data }) => {
+    if (mediaSource.readyState !== "open" || !data.size) {
+      return;
+    }
+    sourceBuffer.appendBuffer(await data.arrayBuffer());
+  };
+  sourceBuffer.addEventListener("update", () => {
+    if (
+      video.buffered.length &&
+      video.buffered.end(0) - video.buffered.start(0) > 35
+    ) {
+      let diff = video.buffered.end(0) - video.buffered.start(0);
+      // Can't remove if video is short
+      sourceBuffer.remove(0, video.buffered.end(0) - 35)
+      log(`REMOVED. ${diff}`);
+    }
+  });
+  video.pause();
+  recorder.start(50);
+  setTimeout(() => video.play(), delay);
+}
+
+startRecording(parseInt(delay_input.value)).then(()=> {}).catch(log);
