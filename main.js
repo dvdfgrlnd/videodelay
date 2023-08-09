6227a224b082bf254384f689888f41eccc1adaba
davidfagerlund <dvdfgrlnd@gmail.com>
2023-08-09 09:20:10 +0200

    Add frequency input slider

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ var source_height = 0;
 var stored_frames = [];
 var starttime = Date.now()
 var stream = null;
+var audioContext = null;
 
 var pause = false;
 
@@ -61,6 +62,34 @@ pause_button.addEventListener("click", () => {
 });
 
 
+const frequency_input = document.querySelector("#frequencyinput");
+const frequency_value = document.querySelector("#frequencyvalue");
+frequency_value.textContent = frequency_input.value;
+frequency_input.addEventListener("input", (event) => {
+  frequency_value.textContent = event.target.value;
+});
+var deadlineTimeout2 = null;
+frequency_input.addEventListener("change", (event) => {
+  if (deadlineTimeout2) {
+    clearTimeout(deadlineTimeout2);
+  }
+  deadlineTimeout2 = setTimeout(() => {
+    let new_delay_seconds = parseInt(event.target.value);
+    if (delay_seconds === new_delay_seconds) {
+      return;
+    }
+    // switchVideoVisibility()
+    // delay_seconds = new_delay_seconds;
+    // console.log(`Delay = ${delay_seconds}`);
+    // stored_frames = [];
+    // starttime = Date.now();
+    // setTimeout(() => {
+    //   switchVideoVisibility()
+    // }, delay_seconds * 1000)
+  }, 1500);
+});
+
+
 const delay_input = document.querySelector("#delayseconds");
 const delay_value = document.querySelector("#delayvalue");
 delay_value.textContent = delay_input.value;
@@ -75,18 +104,18 @@ delay_input.addEventListener("change", (event) => {
     clearTimeout(deadlineTimeout);
   }
   deadlineTimeout = setTimeout(() => {
-    let new_delay_seconds = parseInt(event.target.value);
-    if (delay_seconds === new_delay_seconds) {
-      return;
-    }
-    switchVideoVisibility()
-    delay_seconds = new_delay_seconds;
-    console.log(`Delay = ${delay_seconds}`);
-    stored_frames = [];
-    starttime = Date.now();
-    setTimeout(() => {
-      switchVideoVisibility()
-    }, delay_seconds * 1000)
+    // let new_delay_seconds = parseInt(event.target.value);
+    // if (delay_seconds === new_delay_seconds) {
+    //   return;
+    // }
+    // switchVideoVisibility()
+    // delay_seconds = new_delay_seconds;
+    // console.log(`Delay = ${delay_seconds}`);
+    // stored_frames = [];
+    // starttime = Date.now();
+    // setTimeout(() => {
+    //   switchVideoVisibility()
+    // }, delay_seconds * 1000)
   }, 1500);
 });
 
@@ -101,30 +130,38 @@ function f() {
     c2.putImageData(oldframe, 0, 0);
   }
 
-  if(pause){
+  if (pause) {
     return;
   }
   requestAnimationFrame(f);
 }
 
 function init_stream(e) {
-    console.log("START");
-    video_width = this.videoWidth;
-    video_height = this.videoHeight;
+  console.log("START");
+  video_width = this.videoWidth;
+  video_height = this.videoHeight;
 
-    source_width = video_height * aspect_ratio;
-    source_left = (video_width - source_width) / 2;
-    source_top = 0;
-    source_height = video_height;
+  source_width = video_height * aspect_ratio;
+  source_left = (video_width - source_width) / 2;
+  source_top = 0;
+  source_height = video_height;
 
-    f();
-  }
+  f();
+}
 
-function stop_stream(){
+function stop_stream() {
   stream.getTracks().forEach(track => track.stop());
   video.removeEventListener("loadedmetadata", init_stream);
   video.pause();
   video.currentTime = 0;
+
+  if (audioContext) {
+    audioContext.suspend();
+  }
+  stream.getAudioTracks().forEach(track => {
+    track.stop()
+    this.stream.removeTrack(track);
+  });
 }
 
 async function start_stream() {
@@ -174,100 +211,114 @@ if (window.location.hostname === "localhost" || window.location.hostname === "12
 
 function start_microphone(stream) {
 
-    var audioContext = new AudioContext();
+  audioContext = new AudioContext();
+  let thresholdFrequency = 14000;
 
-    let spectogramEl = document.getElementById("spectogram");
-    let spectogramCtx = spectogramEl.getContext("2d");
-    spectogramCtx.textBaseline = "top";
-    spectogramCtx.font = "12px serif";
+  let spectogramEl = document.getElementById("spectogram");
+  let spectogramCtx = spectogramEl.getContext("2d");
+  spectogramCtx.textBaseline = "top";
+  spectogramCtx.font = "12px serif";
 
-    let WIDTH = spectogramEl.width;
-    let HEIGHT = spectogramEl.height;
+  let WIDTH = spectogramEl.width;
+  let HEIGHT = spectogramEl.height;
 
-    let microphone_stream = audioContext.createMediaStreamSource(stream);
+  let microphone_stream = audioContext.createMediaStreamSource(stream);
 
-    var biquadFilter = audioContext.createBiquadFilter();
-    microphone_stream.connect(biquadFilter);
-    biquadFilter.type = "bandpass";
-    biquadFilter.frequency.value = 14000;
-    biquadFilter.gain.value = 25;
+  var biquadFilter = audioContext.createBiquadFilter();
+  microphone_stream.connect(biquadFilter);
+  biquadFilter.type = "bandpass";
+  biquadFilter.frequency.value = thresholdFrequency;
+  biquadFilter.gain.value = 25;
 
-    biquadFilter.connect(audioContext.destination);
+  biquadFilter.connect(audioContext.destination);
 
-    let analyser_node = audioContext.createAnalyser();
-    analyser_node.smoothingTimeConstant = 0;
-    analyser_node.fftSize = 2048;
+  let analyser_node = audioContext.createAnalyser();
+  analyser_node.smoothingTimeConstant = 0;
+  analyser_node.fftSize = 2048;
 
-    biquadFilter.connect(analyser_node);
+  biquadFilter.connect(analyser_node);
 
-    var buffer_length = analyser_node.frequencyBinCount;
+  var buffer_length = analyser_node.frequencyBinCount;
 
-    var array_freq_domain = new Uint8Array(buffer_length);
+  var array_freq_domain = new Uint8Array(buffer_length);
 
-    console.log("buffer_length " + buffer_length);
+  console.log("buffer_length " + buffer_length);
+  console.log("sample rate = ", audioContext.sampleRate);
+  let freqBinSize = (audioContext.sampleRate / 2) / buffer_length;
+  console.log("freqBinSize = ", freqBinSize);
+  let nonEmptySlots = buffer_length - Math.floor(thresholdFrequency / freqBinSize);
+  console.log("non empty = ", nonEmptySlots);
 
-    var buffer = [];
-    let threshold = 4000;
-    var max = 0;
+  delay_input.max = nonEmptySlots*256;
 
-    var draw = function () {
-        requestAnimationFrame(draw);
+  var buffer = [];
+  let bufferTimeThreshold = 4000;
+  var max = 0;
 
-        analyser_node.getByteFrequencyData(array_freq_domain);
+  var draw = function () {
+    if (pause) {
+      return;
+    }
+    requestAnimationFrame(draw);
 
-        if (microphone_stream.playbackState == microphone_stream.PLAYING_STATE) {
-            let s = array_freq_domain.reduce((acc, v) => v + acc);
+    analyser_node.getByteFrequencyData(array_freq_domain);
 
-            let dn = Date.now();
-            buffer.push([s, dn]);
-            if (buffer[buffer.length - 1][1] - buffer[0][1] > threshold) {
-                buffer.shift()
-            }
+    if (microphone_stream.playbackState == microphone_stream.PLAYING_STATE) {
+      let s = array_freq_domain.reduce((acc, v) => v + acc);
 
-            spectogramCtx.clearRect(0, 0, WIDTH, HEIGHT);
-            spectogramCtx.fillStyle = "rgb(200, 200, 200)";
-            spectogramCtx.fillRect(0, 0, WIDTH, HEIGHT);
-            spectogramCtx.lineWidth = 4;
-            spectogramCtx.strokeStyle = "rgb(0, 0, 0)";
-            spectogramCtx.beginPath();
+      let dn = Date.now();
+      buffer.push([s, dn]);
+      if (buffer[buffer.length - 1][1] - buffer[0][1] > bufferTimeThreshold) {
+        buffer.shift()
+      }
+      if (s > max * 0.7) {
+        console.log("non zero", array_freq_domain.reduce((acc, v) => acc + (v > 0 ? 1 : 0)));
+      }
 
-            max = Math.max(
-                buffer.reduce((acc, v) => Math.max(acc, v[0]), 0),
-                max
-            );
-            spectogramCtx.fillStyle = "rgb(0, 0, 0)";
-            spectogramCtx.fillText(`${max}`, 0, 0);
+      spectogramCtx.clearRect(0, 0, WIDTH, HEIGHT);
+      spectogramCtx.fillStyle = "rgb(200, 200, 200)";
+      spectogramCtx.fillRect(0, 0, WIDTH, HEIGHT);
+      spectogramCtx.lineWidth = 4;
+      spectogramCtx.strokeStyle = "rgb(0, 0, 0)";
+      spectogramCtx.beginPath();
 
-            let ts = buffer[0][1];
-            for (let i = 1; i < buffer.length; i += 1) {
-                let x = ((buffer[i][1] - ts) / threshold) * WIDTH;
-                let v = buffer[i][0];
-                let y = HEIGHT - (v / max) * HEIGHT;
-                if (i === 1) {
-                    spectogramCtx.moveTo(x, y);
-                } else {
-                    spectogramCtx.lineTo(x, y);
-                }
+      max = Math.max(
+        buffer.reduce((acc, v) => Math.max(acc, v[0]), 0),
+        max
+      );
+      spectogramCtx.fillStyle = "rgb(0, 0, 0)";
+      spectogramCtx.fillText(`${max}`, 0, 0);
 
-                if(v > max*0.5){
-                    spectogramCtx.fillText(`${v}`, x*1.01, y*0.95);
-                }
-            }
-
-            spectogramCtx.stroke();
+      let ts = buffer[0][1];
+      for (let i = 1; i < buffer.length; i += 1) {
+        let x = ((buffer[i][1] - ts) / bufferTimeThreshold) * WIDTH;
+        let v = buffer[i][0];
+        let y = HEIGHT - (v / max) * HEIGHT;
+        if (i === 1) {
+          spectogramCtx.moveTo(x, y);
+        } else {
+          spectogramCtx.lineTo(x, y);
         }
 
+        if (v > max * 0.5) {
+          spectogramCtx.fillText(`${v}`, x * 1.01, y * 0.95);
+        }
+      }
+
+      spectogramCtx.stroke();
     }
-    draw();
+
+  }
+  draw();
 }
 
 
-window.onerror = function(msg, url, line, col, error) {
-    let err = "A fatal exception has occurred. The application will now reload.\nException details -> " + msg + "\nUrl: " + url + "\nLine: " + line + '\nColumn: ' + col + '\nError: ' + error;
-    console.error(err);
-    if (!(navigator.userAgent.match("FxiOS") && msg === "Script error.")) {
-      alert(err);
-      location.reload();
-    }
-    return true;
+window.onerror = function (msg, url, line, col, error) {
+  let err = "A fatal exception has occurred. The application will now reload.\nException details -> " + msg + "\nUrl: " + url + "\nLine: " + line + '\nColumn: ' + col + '\nError: ' + error;
+  console.error(err);
+  if (!(navigator.userAgent.match("FxiOS") && msg === "Script error.")) {
+    alert(err);
+    location.reload();
   }
+  return true;
+}
